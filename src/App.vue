e15300eea066f9a940e9bbed0be4efb604a17f7a
Moezz-GH <moezzghoma@yahoo.co.uk>
2025-01-02 01:51:34 +0200

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import { RouterView } from 'vue-router'
      
   </a>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" class="text-white bg-maincolor hover:wihte focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> تواصل معنا </button>
+      <button type="button" class="text-white bg-secondarycolor hover:bg-maincolor focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> تواصل معنا </button>
       <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -25,19 +25,19 @@ import { RouterView } from 'vue-router'
   <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
     <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
-        <a href="/" class="block py-2 px-3 text-white bg-maincolor rounded md:bg-transparent md:text-maincolor md:p-0 md:dark:text-blue-500" aria-current="page">الرئيسية</a>
+         <router-link to="/" exact class="block py-2 px-3 text-gray-900  rounded md:bg-transparent  md:p-0 " active-class="text-secondarycolor">الرئيسية</router-link>
       </li>
       <li>
-        <a href="/learntotrade" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-maincolor md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">تعلم التداول</a>
+       <router-link to="/learntotrade" exact class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-maincolor md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" active-class="text-secondarycolor">تعلم التداول</router-link>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-maincolor md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">مقالات</a>
+        <router-link to="#" exact class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-maincolor md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" active-class="text-secondarycolor">مقالات</router-link>
       </li>
       <li>
-        <a href="/ourservices" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-maincolor md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">خدماتنا</a>
+       <router-link to="/ourservices" exact class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-maincolor md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" active-class="text-secondarycolor">خدماتنا</router-link>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-maincolor md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">من نحن</a>
+        <router-link to="#" exact class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-maincolor md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" active-class="text-secondarycolor">من نحن</router-link>
       </li>
     </ul>
   </div>
--- src/components/OurServices/OurServices.vue
+++ src/components/OurServices/OurServices.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="bg-secondarycolor h-[32rem] bg-center bg-cover bg-no-repeat pt-24">
+  <div>
+  <div class="bg-secondarycolor h-[fit] sm:h-[32rem] bg-center bg-cover bg-no-repeat pt-24">
     <div class="animate__animated animate__backInDown py-18 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-               <p class="my-4 text-5xl font-serif text-white dark:text-gray-400">نحن نفتخر بتقديم خدمات سحب و إيداع سريع   </p>
-               <p class=" my-4 text-5xl font-serif text-white dark:text-gray-400">و تنافسية لتداول العملات الأجنبية من خلال وسيط</p>
-               <p class=" my-4 text-5xl font-serif text-LDN dark:text-gray-400">LDNGlobalMarkets</p>
+               <p class="my-4 text-xl sm:text-5xl font-serif text-white dark:text-gray-400">نحن نفتخر بتقديم خدمات سحب و إيداع سريع   </p>
+               <p class=" my-4 text-xl  sm:text-5xl font-serif text-white dark:text-gray-400">و تنافسية لتداول العملات الأجنبية من خلال وسيط</p>
+               <p class=" my-4 text-xl  sm:text-5xl font-serif text-LDN dark:text-gray-400">LDNGlobalMarkets</p>
         <div class="mt-20 flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">  
         </div>
       
@@ -17,7 +18,7 @@
             <hr>
             <p class="mb-6 font-light text-secondarycolor md:text-lg dark:text-secondarycolor"> أصبحت إدارة أموالك أسهل من أي وقت مضى بفضل خدمات الإيداع و السحب السلسلة لدينا . كجزء من حلول التداول الشاملة اللتي تقدمها لعملاء LDN Broke سوا ءكنت كنت تبحث عن إضافة أموال الى حسابك أو إجراء عملية سحب , فإننا نضمن لك تجربة سريعة و أمنة و خالية من المتاعب</p>
             <p class="mb-6 font-light text-secondarycolor md:text-lg dark:text-secondarycolor">قم بإجراء عمليات التبادل شخصيا في المكتب لمزيد من الأمان و الثقة  </p>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-serif font-medium text-center text-white bg-secondarycolor rounded-lg">
+            <a href="#" class="my-4 inline-flex items-center px-3 py-2 text-sm font-serif font-medium text-center text-white bg-secondarycolor rounded-lg">
             اقرأ المزيد
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -26,7 +27,7 @@
         </div>
 
        <div class="flex items-center justify-center">
-        <img class="shadow-2xl h-[32rem] rounded-2xl hadow-lg w-2/3 dark:hidden" src="https://images.pexels.com/photos/4386433/pexels-photo-4386433.jpeg?auto=compress&cs=tinysrgb&w=600" alt="dashboard image">
+        <img class="shadow-2xl h-[fit] rounded-2xl hadow-lg w-2/3 dark:hidden" src="https://images.pexels.com/photos/4386433/pexels-photo-4386433.jpeg?auto=compress&cs=tinysrgb&w=600" alt="dashboard image">
        </div>
         
 
@@ -34,6 +35,7 @@
         
     </div>
 </section>
+  </div>
 </template>
 
 <script></script>
