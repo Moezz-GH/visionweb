4dbd927b54b044defcc0c653d10c4fbfac8987c1
Moezz-GH <moezzghoma@yahoo.co.uk>
2025-01-02 15:03:47 +0200

    navbar font

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
- <div>
+ <div class="font-costum">
 
 
   <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
