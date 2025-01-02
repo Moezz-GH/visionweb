5c8094553fc55ea6b4cf98c8535df34d7a4bfabc
Moezz-GH <moezzghoma@yahoo.co.uk>
2025-01-02 14:43:47 +0200

    space

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import { RouterView } from 'vue-router'
 
   <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto ">
-  <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+  <a href="#" class="flex items-center space-x-3">
       <img src="../src/assets/logo.png" class="h-24" alt="Tadawal-Logo">
       <span class="self-center text-2xl font-bold font-costum text-secondarycolor whitespace-nowrap dark:text-white">رؤية</span>
     
