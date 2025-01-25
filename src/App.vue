a0b60cc968f86b53251deda0efdfa585951fa88d
SerjoMhana <serjomh33@gmail.com>
2025-01-25 19:35:37 +0200

    SECTIONS

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@ const closeMenu = () => {
             </li>
             <li>
               <router-link
-                to="/Services"
+                to="/#services"
                 @click="closeMenu"
                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-maincolor md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 exact-active-class="text-secondarycolor"
